44a61320ac444853edf375b8aad03f9115118063
Stewart Henderson <stewart.henderson@live.com>
2014-07-21 14:38:35 -0500

    Added Sublime text 2 to editors file.

--- editors.rb
+++ editors.rb
@@ -2,6 +2,11 @@ dep 'Atom.app' do
   source 'https://github.com/atom/atom/releases/download/v0.115.0/atom-mac.zip'
 end
 
+dep "Sublime Text 2.app" do
+  source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg"
+end
+
 dep "editors" do
-  requires "Atom.app"
+  requires "Atom.app",
+           "Sublime Text 2.app"
 end
