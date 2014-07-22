97763aeb45d0d0b7b9d54df19d22ca6d7e8721e7
Stewart Henderson <stewart.henderson@live.com>
2014-07-22 15:03:20 -0500

    corrected bug in plugins

--- intellij-plugins.rb
+++ intellij-plugins.rb
@@ -3,7 +3,7 @@ dep "Scala" do
     "~/Library/Application Support/IntelliJIdea13/Scala".p.exists?
   }
   meet {
-    source "http://plugins.jetbrains.com/files/1347/16634/scala-intellij-bin-0.38.441.zip"
+    shell("cd ~/Library/Application\\ Support/IntelliJIdea13/ && http://plugins.jetbrains.com/files/1347/16634/scala-intellij-bin-0.38.441.zip && unzip scala-intellij-bin-0.38.441.zip")
   }
 end
 
@@ -12,20 +12,21 @@ dep "Lombok" do
     "~/Library/Application Support/IntelliJIdea13/lombok-plugin-0.8.6-13.jar".p.exists?
   }
   meet {
-    source "http://plugins.jetbrains.com/files/6317/15981/lombok-plugin-0.8.6-13.jar"
+    shell("cd ~/Library/Application\\ Support/IntelliJIdea13/ && curl -O http://plugins.jetbrains.com/files/6317/15981/lombok-plugin-0.8.6-13.jar")
   }
 end
 
 dep "ProtoBuf" do
   met? {
-    "~/Library/Application Support/IntelliJIdea13/lombok-plugin-0.8.6-13.jar".p.exists?
+    "~/Library/Application Support/IntelliJIdea13/Google_Protocol_Buffers_support_0.5.7.jar".p.exists?
   }
   meet {
-    source "http://plugins.jetbrains.com/files/6317/15981/lombok-plugin-0.8.6-13.jar"
+    shell("cd ~/Library/Application\\ Support/IntelliJIdea13/ && curl -O http://plugins.jetbrains.com/files/4942/16599/Google_Protocol_Buffers_support_0.5.7.zip && unzip -o Google_Protocol_Buffers_support_0.5.7.zip")
   }
 end
 
 dep "intellij-plugins" do
   requires "Scala",
-           "Lombok"
+           "Lombok",
+           "ProtoBuf"
 end
