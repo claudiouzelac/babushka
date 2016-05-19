4bfa8e631de2f19f34a2f1c0bf0edfa0bfa0c06c
Stewart Henderson <shenderson@channeliq.com>
2016-05-19 15:03:47 -0500

    added cyberghost & hosts file updates

--- os/osx/security.rb
+++ os/osx/security.rb
@@ -14,9 +14,33 @@ dep 'DNSCrypt Menubar.app' do
   source 'https://github.com/alterstep/dnscrypt-osxclient/releases/download/1.0.10/dnscrypt-osxclient-1.0.10.dmg'
 end
 
+dep 'CyberGhost.app' do
+  source 'http://download.cyberghostvpn.com/mac/cg5mac_5.0.14.12.dmg'
+end
+
+def home
+  ENV['HOME']
+end
+
+def hosts_repo
+  File.join(home, 'code', 'hosts')
+end
+
+dep 'hosts' do
+  met? {
+    Babushka::GitRepo.new(hosts_repo).exists?
+  }
+  meet {
+    Babushka::GitRepo.new(hosts_repo).clone! 'https://github.com/StevenBlack/hosts.git'
+    shell "python #{hosts_repo}/updateHostsFile.py --auto"
+  }
+end
+
 dep 'security-osx' do
   requires 'pass.managed',
            'DNSCrypt Menubar.app',
-           'Sync.app'
+           'Sync.app',
+           'hosts',
+           'CyberGhost.app'
            # 'VeraCrypt.app'
 end
