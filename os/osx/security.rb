b710a1d88e9af9654fb4a11239c8670e5f62ccb4
Stewart Henderson <shenderson@channeliq.com>
2016-05-19 15:10:45 -0500

    added VeraCrtpy

--- os/osx/security.rb
+++ os/osx/security.rb
@@ -6,9 +6,12 @@ dep 'keybase.managed' do
   install 'keybase'
 end
 
-# dep 'VeraCrypt.app' do
-#   source 'http://downloads.sourceforge.net/project/veracrypt/VeraCrypt%201.17/VeraCrypt_1.17.dmg?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fveracrypt%2F&ts=1463685515&use_mirror=heanet'
-# end
+dep 'VeraCrypt.installer' do
+  met? {
+    '/Applications/VeraCrypt.app'.p.exists?
+  }
+  source 'http://downloads.sourceforge.net/project/veracrypt/VeraCrypt%201.17/VeraCrypt_1.17.dmg'
+end
 
 dep 'DNSCrypt Menubar.app' do
   source 'https://github.com/alterstep/dnscrypt-osxclient/releases/download/1.0.10/dnscrypt-osxclient-1.0.10.dmg'
@@ -41,6 +44,6 @@ dep 'security-osx' do
            'DNSCrypt Menubar.app',
            'Sync.app',
            'hosts',
-           'CyberGhost.app'
-           # 'VeraCrypt.app'
+           'CyberGhost.app',
+           'VeraCrypt.installer'
 end
