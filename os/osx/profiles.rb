e74455776d5ee5e15ceee81845920225c94f2941
Stewart Henderson <shenderson@channeliq.com>
2016-05-19 19:41:29 -0500

    prevent filevault disable

--- os/osx/profiles.rb
+++ os/osx/profiles.rb
@@ -28,6 +28,23 @@ dep 'suppress_diagnostic_info' do
   }
 end
 
+dep 'cant_disable_filevault' do
+  requires 'profile_directory'
+
+  def diagnostic_info
+    '~/.profiles/cant_disable_filevault.mobileconfig'
+  end
+
+  met? {
+    Babushka::Renderable.new(diagnostic_info).from?('~/.babushka/deps/osx-profiles/cant_disable_filevault.mobileconfig.erb')
+  }
+  meet {
+    render_erb '../../osx-profiles/cant_disable_filevault.mobileconfig.erb', :to => diagnostic_info
+    shell("sudo /usr/bin/profiles -IF #{diagnostic_info}")
+  }
+end
+
 dep 'osx-profiles' do
-  requires 'suppress_diagnostic_info'
+  requires 'suppress_diagnostic_info',
+           'cant_disable_filevault'
 end
