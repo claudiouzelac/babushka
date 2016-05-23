71d3b51b76ce49120ac1f27c1e6c367fc4f4a01b
Stewart Henderson <stewart.henderson@protonmail.com>
2016-05-23 12:51:05 -0500

    added setup of password-store directory

--- os/osx/password-management/pass.rb
+++ os/osx/password-management/pass.rb
@@ -1,3 +1,5 @@
+# https://www.passwordstore.org/
+
 dep 'pass.managed' do
   installs 'pass'
 end
@@ -11,7 +13,21 @@ dep 'qtpass' do
   }
 end
 
+dep 'pass_folder' do
+  met? {
+    '~/.password-store'.p.exists
+  }
+  meet {
+    shell('pass init "ZX2C4 Password Storage Key"')
+    shell('pass git init')
+    #NOTE: you will need to pass git remote add origin {your_repor}
+  }
+end
+
 dep 'pass-osx' do
-  requires 'pass.managed',
-           'qtpass'
+  requires %w(
+              pass.managed
+              qtpass
+              pass_folder
+              )
 end
--- os/osx/privacy/keybase.rb
+++ os/osx/privacy/keybase.rb
@@ -2,6 +2,15 @@ dep 'keybase.managed' do
   installs 'keybase'
 end
 
+# dep 'keybase_configured' do
+#   met?{
+#     '~/.cache/keybase/session.json'
+#   }
+#   meet {
+#     shell('keybase login')
+#   }
+# end
+
 dep 'keybase-osx' do
   requires 'keybase.managed'
 end
