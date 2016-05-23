714ee4c372e0a6200085087b87b5272f296a7030
Stewart Henderson <henderson.geoffrey@gmail.com>
2016-05-23 12:14:32 -0500

    added home dev profile

--- os/osx/software-development/software-development.rb
+++ os/osx/software-development/software-development.rb
@@ -4,3 +4,11 @@ dep 'dev-work-osx' do
            'c-osx',
            'python-osx'
 end
+
+dep 'dev-home-osx' do
+  requires 'git.managed',
+           'git_configured'.with(:git_username => 'Stewart Henderson',:git_email =>'stewart.henderson@protonmail.com'),
+           'c-osx',
+           'python-osx',
+           'buildsystems-osx'
+end
