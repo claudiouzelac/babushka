ae6269af118a0cad696c057f6f69cfd9dd5ff913
Stewart Henderson <stewart.henderson@protonmail.com>
2016-05-23 17:49:24 -0500

    added installation script

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Unset any previous password should the user want to change it
+unset WORK_PASSPHRASE
+
+# This presents the chicken and egg problem
+# In the case that pass isn't installed or the user doesn't have
+# the password then the Babushka script will prompt the user
+# for the password.  This seems acceptable.
+WORK_PASSPHRASE="$(pass work/channeliq/keys/ssh/bitbucket.org)"
+
+babushka meet shenderson-work
