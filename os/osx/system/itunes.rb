9621cc01a188de43b06478cca9269a854e300a97
Stewart Henderson <stewart.henderson@protonmail.com>
2016-05-23 22:36:42 -0500

    cause I dislike iTunes

--- os/osx/system/itunes.rb
+++ os/osx/system/itunes.rb
@@ -6,10 +6,59 @@ dep('disable-genius-sidebar') {
   shell('defaults write com.apple.iTunes disableGeniusSidebar -bool true')
 }
 
+dep('disable-music-store') {
+  shell('defaults write com.apple.iTunes disableMusicStore -bool true')
+}
+
 dep('disable-radio-stations') {
   shell('defaults write com.apple.iTunes disableRadio -bool true')
 }
 
+dep('disable-artist-connect') {
+  shell('defaults write com.apple.iTunes disableArtistConnect -bool true')
+}
+
+dep('dont-automatically-sync-ipods') {
+  shell('defaults write com.apple.iTunes dontAutomaticallySyncIPods -bool true')
+}
+
+dep('disable-itunes-u') {
+  shell('defaults write com.apple.iTunes allowiTunesUAccess -bool false')
+}
+
+dep('disable-podcasts') {
+  shell('defaults write com.apple.iTunes disablePodcasts -bool true')
+}
+
+dep('disable-shared-music') {
+  shell('defaults write com.apple.iTunes disableSharedMusic -bool true')
+}
+
+dep('restrict-movies') {
+  shell('defaults write com.apple.iTunes restrictMovies -bool true')
+}
+
+dep('restrict-explicit-books') {
+  shell('defaults write com.apple.iTunes restrictExplicitBooks -bool true')
+}
+
+dep('restrict-explicit') {
+  shell('defaults write com.apple.iTunes restrictExplicit -bool true')
+}
+
 dep('itunes') {
-  requires %w(disable-itunes-store-arrows disable-genius-sidebar disable-radio-stations)
+  requires %w(
+    disable-itunes-store-arrows
+    disable-genius-sidebar
+    disable-radio-stations
+    disable-artist-connect
+    dont-automatically-sync-ipods
+    disable-itunes-u
+    disable-podcasts
+    disable-shared-music
+    disable-music-store
+    restrict-movies
+    restrict-explicit-books
+    restrict-explicit
+  )
 }
